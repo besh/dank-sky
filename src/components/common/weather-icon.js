ed7e3d2f74d9b0e12ee09238ed9a90c131797164
Dave Beshero <dbesher@us.ibm.com>
2020-04-03 14:43:33 -0400

    feat: better weather icons

--- src/components/common/weather-icon.js
+++ src/components/common/weather-icon.js
@@ -1,25 +1,35 @@
 import React from "react";
-import Icon from "react-native-vector-icons/FontAwesome";
+import Fog from "@carbon/icons/svg/32/fog.svg";
+import Sun from "@carbon/icons/svg/32/sun.svg";
+import Moon from "@carbon/icons/svg/32/moon.svg";
+import Cloudy from "@carbon/icons/svg/32/cloudy.svg";
+import RainHeavy from "@carbon/icons/svg/32/rain--heavy.svg";
+import Sleet from "@carbon/icons/svg/32/sleet.svg";
+import SnowHeavy from "@carbon/icons/svg/32/snow--heavy.svg";
+import Windy from "@carbon/icons/svg/32/windy.svg";
+import PartlyCloudy from "@carbon/icons/svg/32/partly-cloudy.svg";
+import PartlyCloudyNight from "@carbon/icons/svg/32/partly-cloudy--night.svg";
 
 const types = {
-  "clear-day": "sun-o",
-  "clear-night": "moon-o",
-  "partly-cloudy-day": "cloud",
-  "partly-cloudy-night": "cloud",
-  cloudy: "cloud",
-  rain: "shower",
-  sleet: "snowflake-o",
-  snow: "snowflake-o",
-  wind: "cloud",
-  fog: "cloud"
+  "clear-day": Sun,
+  "clear-night": Moon,
+  "partly-cloudy-day": PartlyCloudy,
+  "partly-cloudy-night": PartlyCloudyNight,
+  cloudy: Cloudy,
+  rain: RainHeavy,
+  sleet: Sleet,
+  snow: SnowHeavy,
+  wind: Windy,
+  fog: Fog,
 };
 
 const WeatherIcon = ({ type, size }) => {
-  return <Icon name={types[type]} size={size} />;
+  const Icon = types[type];
+  return <Icon width={size} height={size} />;
 };
 
 WeatherIcon.defaultProps = {
-  size: 32
+  size: 32,
 };
 
 export default WeatherIcon;
